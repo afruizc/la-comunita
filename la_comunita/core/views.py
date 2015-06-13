dae9e4f59191980936fb18192d8db0617537f713
Andres Ruiz <andresfeliperu@gmail.com>
2015-06-13 12:02:34 -0700

    Added basic viewsets for users and communities

--- la_comunita/core/views.py
+++ la_comunita/core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Community
+from .serializers import CommunitySerializer, UserSerializer
+
+
+class UserViewClass(viewsets.UserSerializer):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class CommunityViewSet(viewsets.ModelViewSet):
+    queryset = Community.objects.all()
+    serializer_class = CommunitySerializer
