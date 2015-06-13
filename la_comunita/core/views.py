0197f0ffed276158eccb5ccc880669a085b7cd5f
Andres Ruiz <andresfeliperu@gmail.com>
2015-06-13 13:25:49 -0700

    Added views for all the entities of the app

--- la_comunita/core/views.py
+++ la_comunita/core/views.py
@@ -1,8 +1,10 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from .models import Community, Group
-from .serializers import CommunitySerializer, UserSerializer, GroupSerializer
+from .models import Community, Group, PrivateChat, GroupChat, Message
+from .serializers import (CommunitySerializer, UserSerializer, GroupSerializer,
+                          PrivateChatSerializer, GroupChatSerializer,
+                          MessageSerializer)
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -24,3 +26,21 @@ class GroupViewSet(viewsets.ModelViewSet):
     """View that exposes the API for the groups."""
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class PivateChatViewSet(viewsets.ModelViewSet):
+    """Exposes the API for the private chats."""
+    queryset = PrivateChat.objects.all()
+    serializer_class = PrivateChatSerializer
+
+
+class GroupChatViewSet(viewsets.ModelViewSet):
+    """Exposes API for the group chats."""
+    queryset = GroupChat.objects.all()
+    serializer_class = GroupChatSerializer
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    """Exposes API for messages."""
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
