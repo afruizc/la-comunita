dca4a86c301b7d116b5c51d8c9e0413bd538fa49
Andres Ruiz <andresfeliperu@gmail.com>
2015-06-15 23:03:31 -0700

    Now the user that is logged in is associated with the actions she is doing. For example, when a user creates a group or a chat, she is added as part of that group or set as the sender in the case of a chat.

--- la_comunita/core/views.py
+++ la_comunita/core/views.py
@@ -28,11 +28,22 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
+    def perform_create(self, serializer):
+        """Adds the user that created the group
+        as a member.
+        """
+        g_obj = serializer.save()
+        g_obj.users.add(self.request.user)
+
 
 class ChatViewSet(viewsets.ModelViewSet):
     """Exposes the API for the private chats."""
     queryset = Chat.objects.all()
     serializer_class = ChatSerializer
+
+    def perform_create(self, serializer):
+        c_obj = serializer.save()
+        c_obj.users.add(self.request.user)
 
 
 class MessageViewSet(viewsets.ModelViewSet):
@@ -40,14 +51,26 @@ class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
 
+    def perform_create(self, serializer):
+        """Sets the sender to be the current user."""
+        serializer.save(sender=self.request.user)
+
 
 class GroupInvitationViewSet(viewsets.ModelViewSet):
     """Exposes API for Group Invitations"""
     queryset = GroupInvitation.objects.all()
     serializer_class = GroupInvitationSerializer
 
+    def perform_create(self, serializer):
+        """Sets the inviter to be the current user."""
+        serializer.save(inviter=self.request.user)
+
 
 class ChatInvitationSerializer(viewsets.ModelViewSet):
     """Exposes API for chat invitations"""
     queryset = ChatInvitation.objects.all()
     serializer_class = ChatInvitationSerializer
+
+    def perform_create(self, serializer):
+        """Sets the inviter to be the current user."""
+        serializer.save(inviter=self.request.user)
