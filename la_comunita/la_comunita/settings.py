38850a2a1e3c77b9fb649484087db71b6640dad5
Andres Ruiz <andresfeliperu@gmail.com>
2015-06-18 21:54:05 -0700

    Added nosetests for testing discovery and debugging

--- la_comunita/la_comunita/settings.py
+++ la_comunita/la_comunita/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_swagger',
@@ -115,3 +116,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Tests
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
